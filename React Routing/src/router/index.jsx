46b9a11dbf003d46b99c8158d2ea0fd19b7a7f3f
dhirajnmahajan <dhiraj.mahajan2212@gmail.com>
2026-01-15 13:59:33 +0530

    index file

--- React Routing/src/router/index.jsx
+++ React Routing/src/router/index.jsx
@@ -1,16 +1,14 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import CreateProduct from '../products/create'
-import ProductList from '../products/list'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateProduct from "../products/create";
+import ProductList from "../products/list";
 
-export default function Router (){
-
-
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path='/create' index element={< CreateProduct/>} />
-            <Route path='/' element={< ProductList/>} />
-        </Routes>
-        </BrowserRouter>
-    )
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/create" index element={<CreateProduct />} />
+        <Route path="" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
